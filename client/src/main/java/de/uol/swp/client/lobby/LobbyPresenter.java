a5faf92972dd3c7c0ce17466a8cc9a0a96db0e45
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2020-11-19 13:56:48 +0100

    Lobby View, Presenter und Liste der teilnehmenden User erstellt

--- client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
+++ client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
@@ -1,12 +1,21 @@
 package de.uol.swp.client.lobby;
 
+import com.google.common.eventbus.Subscribe;
 import de.uol.swp.client.AbstractPresenter;
 import de.uol.swp.client.main.MainMenuPresenter;
 import de.uol.swp.common.user.User;
+import de.uol.swp.common.user.UserDTO;
+import de.uol.swp.common.user.response.AllOnlineUsersResponse;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 
 /**
@@ -28,7 +37,29 @@ public class LobbyPresenter extends AbstractPresenter {
 
     private User joinedLobbyUser;
 
+    @FXML
+    private ListView<String> lobbyUsersView;
 
+
+
+    /*@Subscribe
+    public void userList(AllOnlineUsersResponse allUsersResponse) {
+        LOG.debug("Update of user list " + allUsersResponse.getUsers());
+        updateUsersList(allUsersResponse.getUsers());
+    }*/ //TODO:
+
+
+    private void updateUsersList(List<UserDTO> userList) {
+        // Attention: This must be done on the FX Thread!
+        Platform.runLater(() -> {
+            if (lobbyUsers == null) {
+                lobbyUsers = FXCollections.observableArrayList();
+                lobbyUsersView.setItems(lobbyUsers);
+            }
+            lobbyUsers.clear();
+            userList.forEach(u -> lobbyUsers.add(u.getUsername()));
+        });
+    }
 
 
 
