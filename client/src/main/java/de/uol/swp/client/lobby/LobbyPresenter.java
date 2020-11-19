56da12dfc5be7720a810a1eec5de0bea42170f01
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2020-11-19 13:46:35 +0100

    Lobby View und Presenter erstellt

--- client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
+++ client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
@@ -1,7 +1,12 @@
 package de.uol.swp.client.lobby;
 
 import de.uol.swp.client.AbstractPresenter;
+import de.uol.swp.client.main.MainMenuPresenter;
+import de.uol.swp.common.user.User;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 /**
@@ -16,6 +21,14 @@ import javafx.event.ActionEvent;
 public class LobbyPresenter extends AbstractPresenter {
 
     public static final String fxml = "/fxml/LobbyView.fxml";
+
+    private static final Logger LOG = LogManager.getLogger(LobbyPresenter.class);
+
+    private ObservableList<String> lobbyUsers;
+
+    private User joinedLobbyUser;
+
+
 
 
 
