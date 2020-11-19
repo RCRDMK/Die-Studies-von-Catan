94495aa4cf2e09859e584c5841324e1e39c45048
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2020-11-19 13:19:47 +0100

    Lobby View und Presenter erstellt

---
+++ client/src/main/java/de/uol/swp/client/lobby/LobbyPresenter.java
@@ -0,0 +1,31 @@
+package de.uol.swp.client.lobby;
+
+import de.uol.swp.client.AbstractPresenter;
+import javafx.event.ActionEvent;
+
+
+/**
+ * Manages the lobby
+ *
+ * @author Ricardo Mook, Marc Hermes
+ * @see de.uol.swp.client.AbstractPresenter
+ * @since 2020-11-19
+ *
+ */
+
+public class LobbyPresenter extends AbstractPresenter {
+
+    public static final String fxml = "/fxml/LobbyView.fxml";
+
+
+
+
+
+    public void onStartGame(ActionEvent event) {
+        //TODO:
+    }
+
+    public void onLeaveLobby(ActionEvent event) {
+        //TODO:
+    }
+}
