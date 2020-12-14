4aacdf44b3493bdf7e4aaff6d4d66583a552c550
Philip Nitsche <philip.nitsche@uni-oldenburg.de>
2020-12-14 16:06:27 +0100

    Chateingabe wird überprüft ob sie leer ist oder nur blanks enthält und dann direkt eine ResponseEmptyChatMessage geschickt.

--- client/src/main/java/de/uol/swp/client/main/MainMenuPresenter.java
+++ client/src/main/java/de/uol/swp/client/main/MainMenuPresenter.java
@@ -294,10 +294,8 @@ public class MainMenuPresenter extends AbstractPresenter {
             var chatMessage = inputField.getCharacters().toString();
             // ChatID = 0 means main chat
             var chatId = 0;
-            if (!chatMessage.isEmpty() && !chatMessage.equals(null) && !chatMessage.isBlank()) {
-                RequestChatMessage message = new RequestChatMessage(chatMessage, chatId, loggedInUser.getUsername(), System.currentTimeMillis());
-                chatService.sendMessage(message);
-            }
+            RequestChatMessage message = new RequestChatMessage(chatMessage, chatId, loggedInUser.getUsername(), System.currentTimeMillis());
+            chatService.sendMessage(message);
             this.inputField.setText("");
         } catch (Exception e) {
             LOG.debug(e);
