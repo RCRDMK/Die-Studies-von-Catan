c0c2b052d78d87397f174dd9e2d8bd481cbdec4c
Sergej <sergej.tulnev@uni-oldenburg.de>
2021-01-03 15:41:37 +0100

    import

--- server/src/test/java/de/uol/swp/server/usermanagement/AuthenticationServiceTest.java
+++ server/src/test/java/de/uol/swp/server/usermanagement/AuthenticationServiceTest.java
@@ -19,6 +19,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.security.auth.login.LoginException;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
