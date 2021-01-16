cea6ac9ab38744c8ceee4c3baf3afca399669da9
Carsten Dekker <carsten.dekker@uni-oldenburg.de>
2021-01-16 13:58:26 +0100

    -Tests fertig gestellt

--- client/src/test/java/de/uol/swp/client/register/RegistrationServiceTest.java
+++ client/src/test/java/de/uol/swp/client/register/RegistrationServiceTest.java
@@ -2,12 +2,15 @@ package de.uol.swp.client.register;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 /**
- * Test Class for the RegistrationService
- *
+ * Test class for the RegistrationService
+ * <p>
+ * This class tests if we get the right boolean from the isValidEmailAddress method.
  * @author Carsten Dekker
  * @since 2021-01-15
  */
@@ -16,21 +19,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RegistrationServiceTest {
 
     String emailAddress = "test@test.de";
-    String emailAddress2 = "test";
+    String emailAddress1 = "test@test.de ";
+    String emailAddress2 = "test@test.d";
     String emailAddress3 = "";
-    String expectedRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-            "[a-zA-Z0-9_+&*-]+)*@" +
-            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-            "A-Z]{2,7}$";
+    String expectedRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 
     @Test
     public void emailIsValidTest() {
-        assertTrue(emailAddress.matches(expectedRegex));
+        Pattern testPat = Pattern.compile(expectedRegex);
+        assertTrue(testPat.matcher(emailAddress).matches());
     }
 
     @Test
     public void emailIsNotValidTest() {
-        assertFalse(emailAddress2.matches(expectedRegex));
-        assertFalse(emailAddress3.matches(expectedRegex));
+        Pattern testPat1 = Pattern.compile(expectedRegex);
+        assertFalse(testPat1.matcher(emailAddress1).matches());
+        assertFalse(testPat1.matcher(emailAddress2).matches());
+        assertFalse(testPat1.matcher(emailAddress3).matches());
     }
 }
