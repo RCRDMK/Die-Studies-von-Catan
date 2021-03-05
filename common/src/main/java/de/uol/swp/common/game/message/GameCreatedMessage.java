a2b738f2564b2107ecb781489e90f7fb754f6860
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2021-03-05 19:48:46 +0100

    Changes by losse (GameCreatedMessage)

--- common/src/main/java/de/uol/swp/common/game/message/GameCreatedMessage.java
+++ common/src/main/java/de/uol/swp/common/game/message/GameCreatedMessage.java
@@ -1,5 +1,5 @@
 package de.uol.swp.common.game.message;
-
+import de.uol.swp.common.user.UserDTO;
 
 /**
  * Message sent by the server when a user successfully creates a game
@@ -15,8 +15,9 @@ public class GameCreatedMessage extends AbstractGameMessage {
      * Constructor
      * @since 2021-01-07
      */
-    public GameCreatedMessage(String name) {
+    public GameCreatedMessage(String name, UserDTO user) {
         this.name = name;
+        this.user = user;
     }
 
 }
