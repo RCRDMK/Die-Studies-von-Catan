70832fa70be584a1f07b57fcc9b347df5d64a08e
Sergej <sergej.tulnev@uni-oldenburg.de>
2021-05-12 01:00:10 +0200

    + Java Doc nachgereicht + weiter an dem Design und den Regeln gearbeitet

--- client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
+++ client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
@@ -2,6 +2,14 @@ package de.uol.swp.client.game;
 
 import de.uol.swp.client.AbstractPresenter;
 
+
+/**
+ * Manages the Game Rules window
+ *
+ * @author Sergej Tulnev
+ * @see de.uol.swp.client.AbstractPresenter
+ * @since 2021-05-12
+ */
 public class GameRulesPresenter extends AbstractPresenter {
 
     public static final String fxml = "/fxml/GameRulesView.fxml";
--- client/src/main/java/de/uol/swp/client/game/GameRulesService.java
+++ client/src/main/java/de/uol/swp/client/game/GameRulesService.java
@@ -9,12 +9,33 @@ public class GameRulesService {
 
     private final EventBus eventBus;
 
+    /**
+     * Constructor
+     *
+     * @param eventBus The EventBus set in ClientModule
+     * @author Sergej Tulnev
+     * @see de.uol.swp.client.di.ClientModule
+     * @since 2021-05-12
+     */
     @Inject
     public GameRulesService(EventBus eventBus) {
         this.eventBus = eventBus;
         this.eventBus.register(this);
     }
 
+
+    /**
+     * Method called after pressing the Game Rules Button in the main menu scene
+     * <p>
+     * This method creates a new  with the logged in user
+     * and post in on the bus.
+     *
+     * @param user the logged in user
+     * @author Sergej Tulnev
+     * @see RetrieveUserInformationRequest
+     * @see GameRulesPresenter
+     * @since 2021-05-12
+     */
     public void retrieveUserMail(User user) {
         RetrieveUserInformationRequest retrieveUserInformationRequest = new RetrieveUserInformationRequest(user);
         eventBus.post(retrieveUserInformationRequest);
