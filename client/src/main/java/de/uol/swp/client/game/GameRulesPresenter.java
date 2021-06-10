16483ab2da60d9c8dd5f6de212d267d034d880e0
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2021-06-10 17:11:32 +0200

    Game Rules angepasst

--- client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
+++ client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
@@ -2,12 +2,9 @@ package de.uol.swp.client.game;
 
 import de.uol.swp.client.AbstractPresenter;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-import java.awt.*;
 
 
 /**
@@ -22,40 +19,62 @@ public class GameRulesPresenter extends AbstractPresenter {
     public static final String fxml = "/fxml/GameRulesView.fxml";
 
     @FXML
-    ImageView firstRuleImage;
+    ImageView leftPageImage;
 
     @FXML
-    ImageView secondRuleImage;
+    ImageView rightPageImage;
 
 
-    int imagecounter= 00;
+    int imagecounter = 1;
 
+    /**
+     * Handles the Actionevent when the prev button is clicked
+     * <p>
+     * When the prev button is clicked, this method loads the next two images of the game instructions into the Imageviewers.
+     * But only when the imagecounter is greater or equals 3. If it is, the imagecounter will get reduced by 2.
+     *
+     * @author Ricardo Mook
+     * @since 2021-06-10
+     */
+    @FXML
+    public void onPrevButtonClicked() {
+        Platform.runLater(() -> {
+            if (imagecounter >= 3) {
+                imagecounter = imagecounter - 2;
+                Image leftPage = new Image("/img/GameRules/" + imagecounter + ".jpg");
+                leftPageImage.setImage(leftPage);
 
-
-    public void onBackButtonClicked(ActionEvent actionEvent) {
-        if (imagecounter >=02){
-            imagecounter= imagecounter-2;
-            Image leftPage = new Image("src/main/resources/img/GameRules/00" + imagecounter + ".jpg");
-            firstRuleImage.setImage(leftPage);
-
-            Image rightPage = new Image("src/main/resources/img/GameRules/00"+ (imagecounter+1) + ".jpg");
-            secondRuleImage.setImage(leftPage);
-        }
-
-
+                Image rightPage = new Image("/img/GameRules/" + (imagecounter + 1) + ".jpg");
+                rightPageImage.setImage(rightPage);
+            }
+        });
     }
 
-    public void onNextButtonClicked(ActionEvent actionEvent) {
-        if (imagecounter <=10){
-            imagecounter= imagecounter+2;
-            Platform.runLater(()->{
-                Image leftPage = new Image("src/main/resources/img/GameRules/00" + imagecounter + ".jpg");
-                firstRuleImage.setImage(leftPage);
 
-                Image rightPage = new Image("src/main/resources/img/GameRules/00"+ (imagecounter+1) + ".jpg");
-                secondRuleImage.setImage(leftPage);
-            });
+    /**
+     * Handles the actionevent when the next button is pressed.
+     * <p>
+     * When the next button is clicked, this method loads the previous two images of the game instructions into the Imageviewers.
+     * But only when the imagecounter is smaller or equals 13. If it is, the imagecounter will get increased by 2.
+     *
+     * @author Ricardo Mook
+     * @see 2021-06-10
+     */
 
-        }
+    @FXML
+    public void onNextButtonClicked() {
+        Platform.runLater(() -> {
+            if (imagecounter <= 13) {
+                imagecounter = imagecounter + 2;
+
+                Image leftPage = new Image("/img/GameRules/" + imagecounter + ".jpg");
+                leftPageImage.setImage(leftPage);
+
+                Image rightPage = new Image("/img/GameRules/" + (imagecounter + 1) + ".jpg");
+                rightPageImage.setImage(rightPage);
+            }
+        });
     }
 }
+
+
