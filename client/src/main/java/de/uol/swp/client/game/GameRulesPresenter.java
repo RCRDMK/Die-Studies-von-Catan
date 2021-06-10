71d35f6cb57358cf2492b63736cdd0e3538a9b7c
Ricardo <ricardo.mook@uni-oldenburg.de>
2021-06-10 14:50:36 +0200

    .

--- client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
+++ client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
@@ -1,6 +1,13 @@
 package de.uol.swp.client.game;
 
 import de.uol.swp.client.AbstractPresenter;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
 
 
 /**
@@ -14,5 +21,41 @@ public class GameRulesPresenter extends AbstractPresenter {
 
     public static final String fxml = "/fxml/GameRulesView.fxml";
 
+    @FXML
+    ImageView firstRuleImage;
 
+    @FXML
+    ImageView secondRuleImage;
+
+
+    int imagecounter= 00;
+
+
+
+    public void onBackButtonClicked(ActionEvent actionEvent) {
+        if (imagecounter >=02){
+            imagecounter= imagecounter-2;
+            Image leftPage = new Image("src/main/resources/img/GameRules/00" + imagecounter + ".jpg");
+            firstRuleImage.setImage(leftPage);
+
+            Image rightPage = new Image("src/main/resources/img/GameRules/00"+ (imagecounter+1) + ".jpg");
+            secondRuleImage.setImage(leftPage);
+        }
+
+
+    }
+
+    public void onNextButtonClicked(ActionEvent actionEvent) {
+        if (imagecounter <=10){
+            imagecounter= imagecounter+2;
+            Platform.runLater(()->{
+                Image leftPage = new Image("src/main/resources/img/GameRules/00" + imagecounter + ".jpg");
+                firstRuleImage.setImage(leftPage);
+
+                Image rightPage = new Image("src/main/resources/img/GameRules/00"+ (imagecounter+1) + ".jpg");
+                secondRuleImage.setImage(leftPage);
+            });
+
+        }
+    }
 }
