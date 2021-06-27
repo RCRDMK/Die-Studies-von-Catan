e9f2c4c2e1640a45c8cab82b4faf204f893d0a8f
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2021-06-27 16:28:59 +0200

    Methode geschrieben

--- client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
+++ client/src/main/java/de/uol/swp/client/game/GameRulesPresenter.java
@@ -1,22 +1,36 @@
 package de.uol.swp.client.game;
 
 import de.uol.swp.client.AbstractPresenter;
+import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.application.HostServices;
+import javafx.stage.Stage;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 /**
  * Manages the Game Rules window
  *
+ * enhanced by Ricardo Mook, 2021-06-27
+ * made the Presenter extend from Application rather as from the AbstractPresenter, to properly execute a hyperlink
+ *
  * @author Sergej Tulnev
  * @see de.uol.swp.client.AbstractPresenter
  * @since 2021-05-12
  */
-public class GameRulesPresenter extends AbstractPresenter {
+public class GameRulesPresenter extends Application {
 
     public static final String fxml = "/fxml/GameRulesView.fxml";
 
@@ -27,10 +41,23 @@ public class GameRulesPresenter extends AbstractPresenter {
     ImageView rightPageImage;
 
     @FXML
-    Hyperlink link;
+    Hyperlink hyperlink;
 
+    HostServices hostServices = getHostServices();
 
     private int imagecounter = 1;
+
+    /**
+     * Method which must be implemented when extending the Application class. Apart from that it's not used in this
+     * presenter
+     *
+     * @author Ricardo Mook
+     * @since 2021-06-27
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 
     /**
      * Handles the Actionevent when the prev button is clicked
@@ -81,12 +108,19 @@ public class GameRulesPresenter extends AbstractPresenter {
         });
     }
 
-    @FXML
-    public void onGameRulesLinkClicked(ActionEvent actionEvent) {
-        Platform.runLater(()-> {
-            link.setText("http://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/Hyperlink.html");
-        });
+    /**
+     * Handles the actionevent when the hyperlink is clicked
+     * <p>
+     * When hyperlink is clicked, through the hostServices the website for the game appears and the users can download
+     * a copy of the game rules in the form of a PDF file.
+     *
+     * @author Ricardo Mook
+     * @since 2021-06-27
+     */
+    public void onGameRulesLinkClicked() {
+            hostServices.showDocument("https://www.catan.com/service/game-rules");
     }
+
 }
 
 
